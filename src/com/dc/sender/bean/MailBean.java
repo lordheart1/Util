4750bbd8c38e4998cb2189a5aa616241afd77968
lord heart <DELL@DESKTOP-T29ETRU>
2019-11-12 16:02:16 +0800

    MailBean

---
+++ src/com/dc/sender/bean/MailBean.java
@@ -0,0 +1,32 @@
+package com.dc.sender.bean;
+
+import com.dc.sender.annotation.JmsBean;
+
+@JmsBean(type = "111")
+public class MailBean {
+
+	private String content;
+	private String subject;
+	private String recievers;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getRecievers() {
+		return recievers;
+	}
+	public void setRecievers(String recievers) {
+		this.recievers = recievers;
+	}
+	
+	
+}
